7a20ae4bd0eacdbe4c8337060c81209795f38a66
Ivan <me@example.com>
2019-02-11 17:58:25 -0600

    Commit

--- src/AddressForm.js
+++ src/AddressForm.js
@@ -1,98 +1,93 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
 
-function AddressForm() {
+const styles = {
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+};
+
+function AddressForm(props) {
+  const { classes } = props;
   return (
     <React.Fragment>
       <Typography variant="h6" gutterBottom>
-        Shipping address
+        Request information from
       </Typography>
       <Grid container spacing={24}>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="firstName"
-            name="firstName"
-            label="First name"
-            fullWidth
-            autoComplete="fname"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="lastName"
-            name="lastName"
-            label="Last name"
-            fullWidth
-            autoComplete="lname"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            required
-            id="address1"
-            name="address1"
-            label="Address line 1"
-            fullWidth
-            autoComplete="billing address-line1"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            id="addiress2"
-            name="addiress2"
-            label="Address line 2"
-            fullWidth
-            autoComplete="billing address-line2"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="city"
-            name="city"
-            label="City"
-            fullWidth
-            autoComplete="billing address-level2"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField id="state" name="state" label="State/Province/Region" fullWidth />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="zip"
-            name="zip"
-            label="Zip / Postal code"
-            fullWidth
-            autoComplete="billing postal-code"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="country"
-            name="country"
-            label="Country"
-            fullWidth
-            autoComplete="billing country"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <FormControlLabel
-            control={<Checkbox color="secondary" name="saveAddress" value="yes" />}
-            label="Use this address for payment details"
-          />
+        <Grid item xs={12} sm={24}>
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="heart.png"
+          title="Heart"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Dating Applications
+          </Typography>
+          <Typography component="p">
+            Your personality traits, sexual preferences, dating history, and other lifestyle information.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="swimmer-solid.svg.png"
+          title="Swimmer"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Fitness Trackers
+          </Typography>
+          <Typography component="p">
+            Your heartbeat, sleeping patterns, diet, weight, walking habits, and general health.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="satellite-dish-solid.svg.png"
+          title="Satellite"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Telecommunications
+          </Typography>
+          <Typography component="p">
+            Your phone call records, web browsing history, geolocation, and device identifiers.
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
         </Grid>
       </Grid>
     </React.Fragment>
   );
 }
 
-export default AddressForm;
+AddressForm.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(AddressForm);
