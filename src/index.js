2a4d0f1079e7b64795459cbe66bf6359ff23e2c1
Ivan <me@example.com>
2019-02-12 11:51:44 -0600

    Commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Checkout from './Checkout';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+});
 
 class Mui extends React.Component {
   render() {
     return (
       <div>
+<MuiThemeProvider theme={theme}>
         <Checkout />
+</MuiThemeProvider>
       </div>
     );
   }
