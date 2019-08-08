61faa05741c1f6271bc302554a60a225ae5a2b88
Ivan <me@example.com>
2019-08-08 18:24:47 -0500

    Commit

--- src/ProveedoresTelefoniaFija.js
+++ src/ProveedoresTelefoniaFija.js
@@ -25,150 +25,80 @@ function ProveedoresTelefoniaFija(props) {
   return (
     <React.Fragment>
       <Typography variant="h6" gutterBottom>
-        Select your service provider
+        Seleccione su proveedor de servicios
       </Typography>
       <Grid container spacing={24}>
         <Grid item xs={12} sm={24}>
-    <Card className={classes.card}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="telcel.png"
-          title="Amazon"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Amazon
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-    <Card className={classes.card}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="android-brands.svg.png"
-          title="Android"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Android
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-    <Card className={classes.card}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="apple-brands.svg.png"
-          title="Apple"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Apple
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-    <Card className={classes.card}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="bluetooth-brands.svg.png"
-          title="Bluetooth"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Bluetooth
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-    <Card className={classes.card}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="chrome-brands.svg.png"
-          title="Chrome"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Chrome
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-    <Card className={classes.card}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="ebay-brands.svg.png"
-          title="Ebay"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Ebay
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-    <Card className={classes.card}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="firefox-brands.svg.png"
-          title="FireFox"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Firefox
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-    <Card className={classes.card}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="github-brands.svg.png"
-          title="Github"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Github
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-    <Card className={classes.card}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="google-drive-brands.svg.png"
-          title="Google Drive"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Google Drive
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-    <Card className={classes.card}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="gulp-brands.svg.png"
-          title="Gulp"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Gulp
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+          <Card className={classes.card}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image="izzi.png"
+                title="izzi"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  izzi
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+          <Card className={classes.card}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image="maxcom.png"
+                title="Maxcom"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Maxcom
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+          <Card className={classes.card}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image="Megacable.png"
+                title="Megacable"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Megacable
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+          <Card className={classes.card}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image="telmex.png"
+                title="Telmex"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Telmex
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+          <Card className={classes.card}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image="totalplay.png"
+                title="Totalplay"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Totalplay
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
         </Grid>
       </Grid>
     </React.Fragment>
