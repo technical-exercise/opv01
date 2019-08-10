546a62ef81acd876aa2402dd3c6b6e6115adfaea
Ivan <me@example.com>
2019-08-09 21:45:25 -0500

    Commit

--- src/ProveedoresTelefoniaMovil.js
+++ src/ProveedoresTelefoniaMovil.js
@@ -20,117 +20,140 @@ const styles = {
   },
 };
 
-function ProveedoresTelefoniaMovil(props) {
-  const { classes } = props;
-  return (
-    <React.Fragment>
-      <Typography variant="h6" gutterBottom>
-        Seleccione su proveedor de <u>Telefonía móvil</u>
-      </Typography>
-      <Grid container spacing={24}>
-        <Grid item xs={12} sm={24}>
-          <Card className={classes.card}>
-            <CardActionArea>
-              <CardMedia
-                className={classes.media}
-                image="mobile.png"
-                title="Maxcom"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Maxcom
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-          <Card className={classes.card}>
-            <CardActionArea>
-              <CardMedia
-                className={classes.media}
-                image="att.png"
-                title="AT&T"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  AT&T
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-          <Card className={classes.card}>
-            <CardActionArea>
-              <CardMedia
-                className={classes.media}
-                image="weex.png"
-                title="Weex"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Weex
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-          <Card className={classes.card}>
-            <CardActionArea>
-              <CardMedia
-                className={classes.media}
-                image="mobile.png"
-                title="QBOcel"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  QBOcel
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-          <Card className={classes.card}>
-            <CardActionArea>
-              <CardMedia
-                className={classes.media}
-                image="telcel.png"
-                title="TelCel"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  TelCel
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-          <Card className={classes.card}>
-            <CardActionArea>
-              <CardMedia
-                className={classes.media}
-                image="telefonica.png"
-                title="Telefónica"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Telefónica
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-          <Card className={classes.card}>
-            <CardActionArea>
-              <CardMedia
-                className={classes.media}
-                image="virgin.png"
-                title="Virgin Mobile"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Virgin Mobile
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
+class ProveedoresTelefoniaMovil extends React.Component {
+  constructor(props) {
+    super(props);
+    this.goToForm = this.goToForm.bind(this);
+  }
+  goToForm() {
+    this.props.goToForm();
+  }
+  render() {
+    const classes = this.props.classes;
+    return (
+      <React.Fragment>
+        <Typography variant="h6" gutterBottom>
+          Seleccione su proveedor de <u>Telefonía móvil</u>
+        </Typography>
+        <Grid container spacing={24}>
+          <Grid item xs={12} sm={24}>
+            <Card className={classes.card}
+              onClick={this.goToForm}
+            >
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image="mobile.png"
+                  title="Maxcom"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Maxcom
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+            <Card className={classes.card}
+              onClick={this.goToForm}
+            >
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image="att.png"
+                  title="AT&T"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    AT&T
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+            <Card className={classes.card}
+              onClick={this.goToForm}
+            >
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image="weex.png"
+                  title="Weex"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Weex
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+            <Card className={classes.card}
+              onClick={this.goToForm}
+            >
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image="mobile.png"
+                  title="QBOcel"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    QBOcel
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+            <Card className={classes.card}
+              onClick={this.goToForm}
+            >
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image="telcel.png"
+                  title="TelCel"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    TelCel
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+            <Card className={classes.card}
+              onClick={this.goToForm}
+            >
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image="telefonica.png"
+                  title="Telefónica"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Telefónica
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+            <Card className={classes.card}
+              onClick={this.goToForm}
+            >
+              <CardActionArea>
+                <CardMedia
+                  className={classes.media}
+                  image="virgin.png"
+                  title="Virgin Mobile"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Virgin Mobile
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          </Grid>
         </Grid>
-      </Grid>
-    </React.Fragment>
-  );
+      </React.Fragment>
+    );
+  }
 }
 
 export default withStyles(styles)(ProveedoresTelefoniaMovil);
